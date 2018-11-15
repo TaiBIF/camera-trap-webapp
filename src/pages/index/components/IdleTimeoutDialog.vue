3c39ed7c3e2a21ad38560c08eb7e51067e5d2737
AJANelson <nelson@aja.comt.w>
2018-11-16 02:18:05 +0800

    - 加上 loading 樣式 - Site.vue 加上 error-message - Site.vue 加上 IdleTimeoutDialog

---
+++ src/pages/index/components/IdleTimeoutDialog.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="modal fade" :class="{'in': open}">
+    <div class="modal-dialog dialog-ui" role="document">
+      <div class="modal-content">
+        <div class="modal-body">
+          <!-- <a class="close" @click="$emit('close')"><i class="icon-remove-sm"></i></a> -->
+
+          <h1 class="text-green">閒置超時，自動返回檢視模式</h1>
+          <p class="text-gray">資料已經幫您自動存檔，您可以重新進入編輯模式繼續作業。</p>
+
+          <a @click="$emit('close')" class="btn btn-orange">確定</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IdleTimeoutDiaglog',
+  props: {
+    open: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
