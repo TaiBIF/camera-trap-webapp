c49e83f107997da0991d25e37f2f998bed1ba60e
diabloevagto <diabloevagto@gmail.com>
2018-11-20 00:37:04 +0800

    fix: eslint 在修復逗點及分號會多一個錯誤

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,17 +9,6 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
-    semi: ['error', 'always'],
-    'comma-dangle': [
-      'error',
-      {
-        arrays: 'always-multiline',
-        objects: 'always-multiline',
-        imports: 'always-multiline',
-        exports: 'always-multiline',
-        functions: 'always-multiline',
-      },
-    ],
   },
   parserOptions: {
     parser: 'babel-eslint',
