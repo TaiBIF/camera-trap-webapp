144ffded2341229699bbd352446cb80a52527e31
Kelp <kelp@phate.org>
2018-12-26 15:42:43 +0800

    Changed authentication flow. Refresh the session every 20 minutes. #158
    https://github.com/TaiBIF/camera-trap-api/issues/65

--- src/util/auth/cognito-config.js
+++ src/util/auth/cognito-config.js
@@ -16,6 +16,7 @@ AWS.config.update({ region: 'ap-northeast-1' });
 
 // onload
 const auth = initCognitoSDK();
+let refreshIntervalId;
 
 function initCognitoSDK() {
   const result = new CognitoAuth({
@@ -36,21 +37,29 @@ function initCognitoSDK() {
   result.setState('taibif');
   result.userhandler = {
     onSuccess(awsCognitoSession) {
-      const form = document.createElement('form');
-      const inputIdToken = document.createElement('input');
-      const inputRedirect = document.createElement('input');
-      form.method = 'POST';
-      form.action = `${process.env.VUE_APP_API_URL}/ctp-user/sign-in`;
-      inputIdToken.name = 'idToken';
-      inputIdToken.type = 'hidden';
-      inputIdToken.value = awsCognitoSession.getIdToken().getJwtToken();
-      form.appendChild(inputIdToken);
-      inputRedirect.name = 'redirect';
-      inputRedirect.type = 'hidden';
-      inputRedirect.value = location.origin;
-      form.appendChild(inputRedirect);
-      document.body.appendChild(form);
-      form.submit();
+      const logins = {};
+      logins[idpDomain] = awsCognitoSession.getIdToken().getJwtToken();
+      AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: identityPoolId,
+        Logins: logins,
+      });
+      if (!window.currentUser) {
+        const form = document.createElement('form');
+        const inputIdToken = document.createElement('input');
+        const inputRedirect = document.createElement('input');
+        form.method = 'POST';
+        form.action = `${process.env.VUE_APP_API_URL}/ctp-user/sign-in`;
+        inputIdToken.name = 'idToken';
+        inputIdToken.type = 'hidden';
+        inputIdToken.value = awsCognitoSession.getIdToken().getJwtToken();
+        form.appendChild(inputIdToken);
+        inputRedirect.name = 'redirect';
+        inputRedirect.type = 'hidden';
+        inputRedirect.value = location.origin;
+        form.appendChild(inputRedirect);
+        document.body.appendChild(form);
+        form.submit();
+      }
     },
     onFailure(error) {
       console.error(error);
@@ -66,46 +75,43 @@ const authentication = () => {
   Authentication.
   @returns {Promise<CtpUser|null>}
    */
-  return new Promise((resolve, reject) => {
-    if (auth.isUserSignedIn()) {
-      const logins = {};
-      logins[idpDomain] = auth.signInUserSession.idToken.jwtToken;
-      AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-        IdentityPoolId: identityPoolId,
-        Logins: logins,
-      });
-      store
-        .dispatch('auth/loadProfile')
-        .then(() => {
-          const loginUser = store.getters['auth/loginUser'];
-          if (!loginUser || !Object.keys(loginUser).length) {
-            return resolve(null);
-          }
-          resolve(loginUser);
-        })
-        .catch(error => {
-          reject(error);
-        });
-    } else {
-      resolve(null);
-    }
-  }).then(user => {
-    if (user) {
-      // the user is login
-      window.currentUser = user;
-      return user;
-    }
-    if (location.pathname === '/' && location.search.indexOf('code=') >= 0) {
-      // oauth continue
-      auth.parseCognitoWebResponse(location.href);
-      return new Promise();
-    }
-    if (!location.pathname.match(/^\/intro.html/)) {
-      // redirect to login.html
-      window.location.replace('/intro.html');
-      return new Promise();
-    }
-  });
+  if (location.pathname === '/' && location.search.indexOf('code=') >= 0) {
+    // oauth continue
+    auth.parseCognitoWebResponse(location.href);
+    return new Promise(() => {});
+  }
+
+  return store
+    .dispatch('auth/loadProfile')
+    .then(() => {
+      const loginUser = store.getters['auth/loginUser'];
+      if (!loginUser || !Object.keys(loginUser).length) {
+        return null;
+      }
+      return loginUser;
+    })
+    .then(user => {
+      if (auth.isUserSignedIn()) {
+        // The user is login.
+        window.currentUser = user;
+        auth.refreshSession(auth.signInUserSession.refreshToken.refreshToken);
+        if (refreshIntervalId) {
+          clearInterval(refreshIntervalId);
+        }
+        refreshIntervalId = setInterval(() => {
+          auth.refreshSession(auth.signInUserSession.refreshToken.refreshToken);
+        }, 1200 * 1000);
+        return user;
+      }
+
+      // The user didn't login.
+      if (!location.pathname.match(/^\/intro.html/)) {
+        // redirect to login.html
+        window.location.replace('/intro.html');
+        return new Promise(() => {});
+      }
+      return null;
+    });
 };
 
 export { auth, authentication };
